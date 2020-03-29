a14246de1054eb909c3099be78a57253a441e8ef
Jian S <sj8716643@126.com>
2020-03-29 12:41:40 +0800

    Uploaded project 3

---
+++ project_3_Computer Point Controller/README template.md
@@ -0,0 +1,29 @@
+*NOTE:* This file is a template that you can use to create the README for your project. The *TODO* comments below will highlight the information you should be sure to include.
+
+# Computer Pointer Controller
+
+*TODO:* Write a short introduction to your project.
+
+## Project Set Up and Installation
+*TODO:* Explain the setup procedures to run your project. For instance, this can include your project directory structure, the models you need to download and where to place them etc. Also include details about how to install the dependencies your project requires.
+
+## Demo
+*TODO:* Explain how to run a basic demo of your model.
+
+## Documentation
+*TODO:* Include any documentation that users might need to better understand your project code. For instance, this is a good place to explain the command line arguments that your project supports.
+
+## Benchmarks
+*TODO:* Include the benchmark results of running your model on multiple hardwares and multiple model precisions. Your benchmarks can include: model loading time, input/output processing time, model inference time etc.
+
+## Results
+*TODO:* Discuss the benchmark results and explain why you are getting the results you are getting. For instance, explain why there is difference in inference time for FP32, FP16 and INT8 models.
+
+## Standout Suggestions
+*TODO (Optional):* This is where you can provide information about any standout suggestions that you have attempted.
+
+### Async Inference
+*TODO (Optional):* If you have used Async Inference in your code, benchmark the results and explain its effects on power and performance of your project.
+
+### Edge Cases
+*TODO (Optional):* There will be certain situations that will break your inference flow. For instance, lighting changes or multiple people in the frame. Explain some of the edge cases you encountered in your project and how you solved them to make your project more robust.
---
+++ project_3_Computer Point Controller/src/model.py
@@ -0,0 +1,58 @@
+'''
+This is a sample class for a model. You may choose to use it as-is or make any changes to it.
+Since you will be using four models to build this project, you will need to replicate this file
+for each of the models.
+
+This has been provided just to give you an idea of how to structure your model class.
+'''
+
+class Model_X:
+    '''
+    Class for a Model.
+    '''
+    def __init__(self, model_name, device='CPU', extensions=None):
+        '''
+        TODO: Use this to set your instance variables.
+        '''
+        raise NotImplementedError
+
+    def load_model(self):
+        '''
+        TODO: You will need to complete this method.
+        This method is for loading the model to the device specified by the user.
+        If your model requires any Plugins, this is where you can load them.
+        '''
+        raise NotImplementedError
+
+    def predict(self, image):
+        '''
+        TODO: You will need to complete this method.
+        This method is meant for running predictions on the input image.
+        '''
+        raise NotImplementedError
+
+    def check_plugin(self, plugin):
+        '''
+        TODO: You will need to complete this method as a part of the 
+        standout suggestions
+
+        This method checks whether the model(along with the plugin) is supported
+        on the CPU device or not. If not, then this raises and Exception
+        '''
+        raise NotImplementedError
+
+    def preprocess_input(self, image):
+        '''
+        TODO: You will need to complete this method.
+        Before feeding the data into the model for inference,
+        you might have to preprocess it. This function is where you can do that.
+        '''
+        raise NotImplementedError
+
+    def preprocess_output(self, outputs):
+        '''
+        TODO: You will need to complete this method.
+        Before feeding the output of this model to the next model,
+        you might have to preprocess the output. This function is where you can do that.
+        '''
+        raise NotImplementedError
